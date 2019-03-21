7f5ce27adef8e75ec6fb3bb07e2bee8198ae757b
didin jamaludin <didinj@gmail.com>
2019-03-21 14:46:55 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Node.js, Express.js, and Multer Image Uploader Example
+
+This source code use for Ionic 4 Image Crop and Uploader Example
---
+++ routes/index.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+var multer  = require('multer');
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, './public/images');
+    },
+    filename: (req, file, cb) => {
+      console.log(file);
+      var filetype = '';
+      if(file.mimetype === 'image/gif') {
+        filetype = 'gif';
+      }
+      if(file.mimetype === 'image/png') {
+        filetype = 'png';
+      }
+      if(file.mimetype === 'image/jpeg') {
+        filetype = 'jpg';
+      }
+      cb(null, 'image-' + Date.now() + '.' + filetype);
+    }
+});
+var upload = multer({storage: storage});
+
+/* GET home page. */
+router.post('/', function(req, res, next) {
+  Product.create(req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+router.post('/upload',upload.single('file'),function(req, res, next) {
+  console.log(req.file);
+  if(!req.file) {
+    res.status(500);
+    return next(err);
+  }
+  res.json({ fileUrl: 'http://192.168.0.7:3000/images/' + req.file.filename });
+})
+
+module.exports = router;
